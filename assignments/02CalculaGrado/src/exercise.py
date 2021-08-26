461f99d638fc4e83260b35a20c4357e3abaa06b6
Angel Perez <a00833469@itesm.mx>
2021-08-26 16:30:54 -0500

    Trabajo Funciones basicas

--- assignments/02CalculaGrado/src/exercise.py
+++ assignments/02CalculaGrado/src/exercise.py
@@ -1,3 +1,4 @@
+print("Hello World!")
 def calcula_grado(grado):
     if grado < 0.0 or grado > 1.0:
         nota = "score incorrecto"
--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,5 +1,10 @@
-# Escribe aquí tus funciones...
+def arearectan (a, b):
+    return a *b 
 
+def volumen_prisma(base, altura, profundidad):
+    volumen = arearectan(base, altura) * profundidad
+    return volumen
+    
 def main():
     #escribe tu código abajo de esta línea
     b = float(input("Dame la base: "))
@@ -7,8 +12,8 @@ def main():
     p = float(input("Dame la profundidad: "))
 
     r = volumen_prisma(b,a,p)
-
     print("El volumen del prisma es:",r)
+   
 
 if __name__=='__main__':
     main()
